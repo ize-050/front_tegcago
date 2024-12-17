6dbf112a39863602f52470df835b465767b7a020
sukit jettananusart <sukit@uppercuz.com>
2024-12-17 22:17:07 +0700

    update

--- src/components/Content/Prepurchase/viewPurchase.tsx
+++ src/components/Content/Prepurchase/viewPurchase.tsx
@@ -33,6 +33,7 @@ import Lucide from "@/components/Base/Lucide";
 import ModalPreviewImageView from "./ModelviewImage";
 import ModalPreviewImage from "@/components/Content/Prepurchase/upload/ModalPreview";
 import { MapProvider } from "@/components/Map/Mapprovider";
+import ViewImageComponent from "@/components/CS/Content/StatusPurchase/Image/ViewImageComponent";
 
 const ViewPrePurchase = ({ purchase }: any) => {
   const { customer_detail } = useAppSelector(customerData);
@@ -175,132 +176,19 @@ const ViewPrePurchase = ({ purchase }: any) => {
                 const isImage = images.name?.endsWith('.jpg') || images.name?.endsWith('.png') || images.name?.endsWith('.jpeg') || images.name?.endsWith('.webp');;
                 const url = images.url;
                 return (
-                  <div
-                    key={index}
-                    className="relative w-32 h-32 m-2 basis-1/4 overflow-hidden"
-                  >
-                    {isPdf && (
-                      <>
-                        <div className="relative w-full h-full   overflow-hidden">
-                          <object
-                            data={url}
-                            type="application/pdf"
-                            height={"100%"}
-                            width={"100%"}
-                          >
-                            <div className="flex items-center justify-center h-full">
-                              <p className="text-gray-500">
-                                PDF Viewer not available
-                              </p>
-                            </div>
-                          </object>
-                          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                            <span className="text-white text-lg font-medium">
-                              PDF
-                            </span>
-                          </div>
-
-                          <div className="absolute bottom-1 right-0 flex gap-2">
-                            <button
-                              onClick={() => {
-                                window.open(images.url);
-                              }}
-                              type="button"
-                              className="hover:bg-blue-300 bg-[#C8D9E3] w-6 h-6 rounded-lg mr-1"
-                            >
-                              <Lucide
-                                color="#6C9AB5"
-                                icon="Eye"
-                                className="w-5 h-5 m-auto"
-                              />
-                            </button>
-                          </div>
-                        </div>
-                      </>
-                    )}
-
-                    {isImage && (
-                      <>
-                        <div className="relative w-full h-full   overflow-hidden">
-                          <Image
-                            src={url}
-                            alt={`Preview ${index}`}
-                            fill
-                            className="w-full h-full object-cover rounded"
-                            onError={(e) => {
-                              // Placeholder or error message on image load failure
-                              e.currentTarget.src = "/images/placeholder.jpg";
-                            }}
-                          />
-                          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                            <span className="text-white text-lg font-medium">
-                              image
-                            </span>
-                          </div>
-                          <div className="absolute bottom-1 right-0 flex gap-2">
-                            <button
-                              onClick={() => {
-                                dispatch(setModalImage(true));
-                                setSelectedImageIndex(index);
-                              }} // Consider passing the  image (url, index, etc.)
-                              type="button"
-                              className="hover:bg-blue-300 bg-[#C8D9E3] w-6 h-6 rounded-lg mr-1"
-                            >
-                              <Lucide
-                                color="#6C9AB5"
-                                icon="Eye"
-                                className="w-5 h-5 m-auto"
-                              />
-                            </button>
-                          </div>
-                          {/* Pass data or URL to the ModalPreviewImage component */}
-                          {/* <ModalPreviewImage ... /> */}
-                        </div>
-                        {modalImage && selectIndex === index && (
-                          <ModalPreviewImage
-                            isOpen={modalImage}
-                            onClose={() => dispatch(setModalImage(false))}
-                            startIndex={index}
-                            images={data?.d_product?.d_product_image}
-                          />
-                        )}
-                      </>
-                    )}
-
-                    {isExcel && (
-                      <div>
-                        <div className="relative w-full h-32  border-2   overflow-hidden">
-                          <Lucide
-                            icon="Sheet"
-                            className="absolute top-10 left-6 w-8 h-8 mx-auto text-green-500"
-                          />
-                          <h3 className="text-sm font-semibold mb-2">
-                            {images.name}
-                          </h3>
-                          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-                            <span className="text-white text-lg font-medium">
-                              Excel
-                            </span>
-                          </div>
-                          <div className="absolute bottom-1 right-0 flex gap-2">
-                            <button
-                              onClick={() => {
-                                window.open(images.url);
-                              }}
-                              type="button"
-                              className="hover:bg-blue-300 bg-[#C8D9E3] w-6 h-6 rounded-lg mr-1"
-                            >
-                              <Lucide
-                                color="#6C9AB5"
-                                icon="Eye"
-                                className="w-5 h-5 m-auto"
-                              />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    )}
-                  </div>
+                  <>
+                  <ViewImageComponent 
+                  
+                  url={url}
+                   isExcel={isExcel}
+                   isPdf={isPdf}
+                   isImage={isImage}
+                   images={images}
+                   index={index}
+                   key={index}
+                   
+                  />
+                  </>
                 );
               }
             )}
